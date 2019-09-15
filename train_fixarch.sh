61fec44901ecb110fa6780bafefcd9893344a4bc
Yan Kang <kangyan.cn@gmail.com>
2019-09-14 17:34:47 -0700

    train shell

--- train_fixarch.sh
+++ train_fixarch.sh
@@ -3,8 +3,8 @@ export MXNET_SAFE_ACCUMULATION=1
 python train_imagenet.py \
     --rec-train ~/imagenet/rec/train.rec --rec-train-idx ~/imagenet/rec/train.idx \
     --rec-val ~/imagenet/rec/val.rec --rec-val-idx ~/imagenet/rec/val.idx \
-    --model ShuffleNas_fixArch --mode imperactive \
-    --lr 0.05 --wd 0.00004 --lr-mode cosine --dtype float32\
-    --num-epochs 150 --batch-size 96 --num-gpus 4 -j 4 \
-    --label-smoothing --no-wd --warmup-epochs 5 --use-rec \
-    --save-dir params_shufflenas_fixarch --logging-file shufflenas_fixarch.log
+    --model ShuffleNas_fixArch --mode hybrid \
+    --lr 0.5 --wd 0.00004 --lr-mode cosine --dtype float16\
+    --num-epochs 307 --batch-size 256 --num-gpus 4 -j 4 \
+    --label-smoothing --no-wd --warmup-epochs 10 --use-rec \
+    --save-dir params_shufflenas_fixarch1024 --logging-file shufflenas_fixarch1024.log
