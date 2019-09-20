0ba5417071c612066a47d41072bdcbdd3c1b626c
Alex <kangyan.cn@gmail.com>
2019-09-19 18:03:00 -0700

    Update train_fixarch.sh

--- train_fixarch.sh
+++ train_fixarch.sh
@@ -5,7 +5,7 @@ python train_imagenet.py \
     --rec-val ~/imagenet/rec/val.rec --rec-val-idx ~/imagenet/rec/val.idx \
     --model ShuffleNas_fixArch --mode hybrid \
     --lr 0.5 --wd 0.00004 --lr-mode cosine --dtype float16\
-    --num-epochs 240 --batch-size 256 --num-gpus 4 -j 4 \
+    --num-epochs 240 --batch-size 256 --num-gpus 4 -j 8 \
     --label-smoothing --no-wd --warmup-epochs 10 --use-rec \
-    --save-dir params_shufflenas_fixarch1024 --logging-file shufflenas_fixarch1024.log \
+    --save-dir params_shufflenas_fixarch --logging-file shufflenas_fixarch.log \
     --use-se --last-conv-after-pooling
