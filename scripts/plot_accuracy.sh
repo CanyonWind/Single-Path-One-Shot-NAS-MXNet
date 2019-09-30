a2c52de45d5ba1131c13f7753f973f9529214440
Alex <kangyan.cn@gmail.com>
2019-09-30 15:50:45 -0700

    Update plot_accuracy.sh

--- scripts/plot_accuracy.sh
+++ scripts/plot_accuracy.sh
@@ -1,3 +1,5 @@
 python plot.py \
     --log-file ./logs/shufflenas_supernet.log \
-    --mode accuracy
+    --mode accuracy \
+    --title "Supernet top-1 accuracy" \
+    --save-file ./supernet_acc.png
