05863d3721d98deeb42321265214e1e85222e24f
Alex <kangyan.cn@gmail.com>
2019-10-02 14:27:09 -0700

    Update search_supernet.sh

--- scripts/search_supernet.sh
+++ scripts/search_supernet.sh
@@ -1,10 +1,10 @@
 python search_supernet.py \
     --rec-train ~/imagenet/rec/train.rec --rec-train-idx ~/imagenet/rec/train.idx \
     --rec-val ~/imagenet/rec/val.rec --rec-val-idx ~/imagenet/rec/val.idx \
-    --batch-size 128 --num-gpus 4 -j 8 \
-    --supernet_params ./params/ShuffleNasOneshot-imagenet-supernet.params \
+    --batch-size 128 --num-gpus 4 --num-workers 8 \
+    --supernet-params ./params/ShuffleNasOneshot-imagenet-supernet.params \
     --dtype float32 --shuffle-train False \
     --search-mode genetic --comparison-model SinglePathOneShot \
-    --topk 3 --search_iters 10 --update_bn_images 20000\
-    --population_size 500 --retain_length 100 \
-    --random_select 0.1 --mutate_chance 0.1
+    --topk 3 --search-iters 20 --update-bn-images 20000\
+    --population-size 50 --retain-length 10 \
+    --random-select 0.1 --mutate-chance 0.1
