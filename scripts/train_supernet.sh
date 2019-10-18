b783f5d93b099c184797b60bf081385c03bff44c
Alex <kangyan.cn@gmail.com>
2019-10-17 17:38:38 -0700

    Update train_supernet.sh

--- scripts/train_supernet.sh
+++ scripts/train_supernet.sh
@@ -4,8 +4,8 @@ python train_imagenet.py \
     --rec-train ~/imagenet/rec/train.rec --rec-train-idx ~/imagenet/rec/train.idx \
     --rec-val ~/imagenet/rec/val.rec --rec-val-idx ~/imagenet/rec/val.idx \
     --model ShuffleNas --mode imperative \
-    --lr 0.25 --wd 0.00004 --lr-mode cosine --dtype float16\
-    --num-epochs 120 --batch-size 128 --num-gpus 4 -j 4 \
-    --label-smoothing --no-wd --warmup-epochs 10 --use-rec \
-    --save-dir params_shufflenas_supernet --logging-file ./logs/shufflenas_supernet.log \
-    --epoch-start-cs 60 --cs-warm-up --use-se --last-conv-after-pooling
+    --lr 0.65 --wd 0.00004 --lr-mode cosine --dtype float16\
+    --num-epochs 120 --batch-size 128 --num-gpus 4 -j 16 \
+    --label-smoothing --no-wd --warmup-epochs 5 --use-rec \
+    --save-dir params_shufflenas_v2+_supernet --logging-file ./logs/shufflenas_v2+_supernet.log \
+    --epoch-start-cs 30 --cs-warm-up --use-se --last-conv-after-pooling --channels-layout ShuffleNetV2+
