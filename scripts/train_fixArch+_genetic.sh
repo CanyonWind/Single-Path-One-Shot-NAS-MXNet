0b1ac24bf12913539ff22ab4df95542887da8c76
Alex <kangyan.cn@gmail.com>
2019-10-18 15:35:21 -0700

    Update train_fixArch+_genetic.sh

--- scripts/train_fixArch+_genetic.sh
+++ scripts/train_fixArch+_genetic.sh
@@ -5,7 +5,7 @@ python train_imagenet.py \
     --rec-val ~/imagenet/rec/val.rec --rec-val-idx ~/imagenet/rec/val.idx \
     --model ShuffleNas_fixArch --mode hybrid \
     --lr 1.3 --wd 0.00003 --lr-mode cosine --dtype float16\
-    --num-epochs 360 --batch-size 256 --num-gpus 4 -j 8 \
+    --num-epochs 360 --batch-size 256 --num-gpus 4 -j 16 \
     --label-smoothing --no-wd --warmup-epochs 5 --use-rec \
-    --use-se --last-conv-after-pooling \
+    --use-se --channels-layout ShuffleNetV2+ \
     --save-dir params_shufflenas_oneshot+_genetic --logging-file ./logs/shufflenas_oneshot+_genetic.log
