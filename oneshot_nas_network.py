d08713b98f2288ff32a6eb63f2a0a99d88dd049f
Alex <kangyan.cn@gmail.com>
2019-10-17 17:34:32 -0700

    Update oneshot_nas_network.py

--- oneshot_nas_network.py
+++ oneshot_nas_network.py
@@ -4,12 +4,13 @@ from mxnet.gluon.nn import HybridBlock
 from mxnet import nd
 import os
 from sys import maxsize
-
 import random
+import sys
+import numpy as np
+
 from oneshot_nas_blocks import NasHybridSequential, ShuffleNetBlock, ShuffleNasBlock, NasBatchNorm, Activation, SE, \
                                ShuffleChannelsConv, ShuffleChannels
 
-import sys
 sys.path.append('./utils')
 from calculate_flops import get_flops
 
@@ -20,7 +21,8 @@ __all__ = ['get_shufflenas_oneshot', 'ShuffleNasOneShot', 'ShuffleNasOneShotFix'
 class ShuffleNasOneShot(HybridBlock):
     def __init__(self, input_size=224, n_class=1000, architecture=None, channel_scales=None,
                  use_all_blocks=False, bn=nn.BatchNorm, use_se=False, last_conv_after_pooling=False,
-                 shuffle_method=ShuffleChannels):
+                 shuffle_method=ShuffleChannels, stage_out_channels=None, candidate_scales=None,
+                 last_conv_out_channel=1024):
         """
         scale_cand_ids = [6, 5, 3, 5, 2, 6, 3, 4, 2, 5, 7, 5, 4, 6, 7, 4, 4, 5, 4, 3]
         scale_candidate_list = [0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0]
@@ -30,13 +32,14 @@ class ShuffleNasOneShot(HybridBlock):
         super(ShuffleNasOneShot, self).__init__()
         # Predefined
         self.stage_repeats = [4, 4, 8, 4]
-        self.stage_out_channels = [64, 160, 320, 640]
-        self.candidate_scales = [0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0]
+        self.stage_out_channels = [64, 160, 320, 640] if stage_out_channels is None else stage_out_channels
+        self.candidate_scales = [0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0] \
+            if candidate_scales is None else candidate_scales
         self.use_all_blocks = use_all_blocks
         self.use_se = use_se
 
         first_conv_out_channel = 16
-        last_conv_out_channel = 1024
+        last_conv_out_channel = last_conv_out_channel
         self.last_conv_after_pooling = last_conv_after_pooling
 
         if architecture is None and channel_scales is None:
@@ -78,10 +81,10 @@ class ShuffleNasOneShot(HybridBlock):
                     # create repeated blocks for current stage
                     for i in range(numrepeat):
                         stride = 2 if i == 0 else 1
-                        # TODO: update SE and Activation in ShuffleNetBlock and ShuffleNasBlock
                         if fix_arch:
                             block_choice = architecture[block_id]
-                            mid_channel = int(output_channel // 2 * channel_scales[block_id])
+                            mid_channel = make_divisible(int(output_channel // 2 * channel_scales[block_id]))
+                            # print("Block {} mid channel: {}".format(block_id, mid_channel))
                             # print("Mid channel: {}".format(mid_channel))
                             block_id += 1
                             if block_choice == 0:
@@ -123,7 +126,7 @@ class ShuffleNasOneShot(HybridBlock):
                         nn.GlobalAvgPool2D(),
                         # no last SE for MobileNet V3 style
                         nn.Conv2D(last_conv_out_channel, in_channels=input_channel, kernel_size=1, strides=1,
-                                  padding=0, use_bias=True, prefix='conv_fc_'),
+                                  padding=0, use_bias=False, prefix='conv_fc_'),
                         # No bn for the conv after pooling
                         Activation('hard_swish' if self.use_se else 'relu')
                     )
@@ -173,14 +176,13 @@ class ShuffleNasOneShot(HybridBlock):
         return nd.array(block_choices).astype(dtype, copy=False)
 
     def random_channel_mask(self, select_all_channels=False, dtype='float32', mode='sparse', epoch_after_cs=maxsize,
-                            ignore_first_two_cs=True):
+                            ignore_first_two_cs=False):
         """
         candidate_scales = [0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0]
         mode: str, "dense" or "sparse". Sparse mode select # channel from candidate scales. Dense mode selects
               # channels between randint(min_channel, max_channel).
         """
         assert len(self.stage_repeats) == len(self.stage_out_channels)
-
         # From [1.0, 1.2, 1.4, 1.6, 1.8, 2.0] to [0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0], warm-up stages are
         # not just 1 epoch, but 2, 3, 4, 5 accordingly.
         if 0 <= epoch_after_cs <= 23:
@@ -205,22 +207,20 @@ class ShuffleNasOneShot(HybridBlock):
 
         channel_mask = []
         channel_choices = []
-        global_max_length = int(self.stage_out_channels[-1] // 2 * self.candidate_scales[-1])
+        global_max_length = make_divisible(int(self.stage_out_channels[-1] // 2 * self.candidate_scales[-1]))
         for i in range(len(self.stage_out_channels)):
-            local_max_length = int(self.stage_out_channels[i] // 2 * self.candidate_scales[-1])
-            local_min_length = int(self.stage_out_channels[i] // 2 * self.candidate_scales[0])
+            local_max_length = make_divisible(int(self.stage_out_channels[i] // 2 * self.candidate_scales[-1]))
+            local_min_length = make_divisible(int(self.stage_out_channels[i] // 2 * self.candidate_scales[0]))
             for _ in range(self.stage_repeats[i]):
                 if select_all_channels:
                     local_mask = [1] * global_max_length
                 else:
                     local_mask = [0] * global_max_length
-                    # TODO: shouldn't random between min and max. But select candidate scales and return it.
                     if mode == 'dense':
                         random_select_channel = random.randint(local_min_length, local_max_length)
                         # In dense mode, channel_choices is # channel
                         channel_choices.append(random_select_channel)
                     elif mode == 'sparse':
-
                         # this is for channel selection warm up: channel choice ~ (8, 9) -> (7, 9) -> ... -> (0, 9)
                         channel_scale_start = max(min_scale_id, len(self.candidate_scales) - delayed_epoch_after_cs - 2)
                         channel_choice = random.randint(channel_scale_start, len(self.candidate_scales) - 1)
@@ -228,6 +228,8 @@ class ShuffleNasOneShot(HybridBlock):
                                                     self.candidate_scales[channel_choice])
                         # In sparse mode, channel_choices is the indices of candidate_scales
                         channel_choices.append(channel_choice)
+                        # To take full advantages of acceleration, # of channels should be divisible to 8.
+                        random_select_channel = make_divisible(random_select_channel)
                     else:
                         raise ValueError("Unrecognized mode: {}".format(mode))
                     for j in range(random_select_channel):
@@ -263,6 +265,30 @@ class ShuffleNasOneShot(HybridBlock):
         return x
 
 
+class ShuffleNasOneShotFix(ShuffleNasOneShot):
+    # Unlike its parent class, fix-arch model does not have the control of "use_all_blocks" and "bn"(for NasBN).
+    # It should use the default False and nn.BatchNorm correspondingly.
+    def __init__(self, input_size=224, n_class=1000, architecture=None, channel_scales=None,
+                 use_se=False, last_conv_after_pooling=False, shuffle_method=ShuffleChannels,
+                 stage_out_channels=None, candidate_scales=None, last_conv_out_channel=1024):
+        """
+        scale_cand_ids = [6, 5, 3, 5, 2, 6, 3, 4, 2, 5, 7, 5, 4, 6, 7, 4, 4, 5, 4, 3]
+        scale_candidate_list = [0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0]
+        stage_repeats = [4, 4, 8, 4]
+        len(scale_cand_ids) == sum(stage_repeats) == # feature blocks == 20
+        """
+        super(ShuffleNasOneShotFix, self).__init__(input_size=input_size, n_class=n_class,
+                                                   architecture=architecture, channel_scales=channel_scales,
+                                                   use_se=use_se, last_conv_after_pooling=last_conv_after_pooling,
+                                                   shuffle_method=shuffle_method, stage_out_channels=stage_out_channels,
+                                                   candidate_scales=candidate_scales, last_conv_out_channel=last_conv_out_channel)
+
+    def hybrid_forward(self, F, x, *args, **kwargs):
+        x = self.features(x)
+        x = self.output(x)
+        return x
+
+
 def generate_transpose_conv_kernel(channels):
     c = channels
     if c % 2 != 0:
@@ -275,34 +301,31 @@ def generate_transpose_conv_kernel(channels):
     return nd.expand_dims(nd.expand_dims(nd.array(weights), axis=2), axis=3)
 
 
-class ShuffleNasOneShotFix(ShuffleNasOneShot):
-    # Unlike its parent class, fix-arch model does not have the control of "use_all_blocks" and "bn"(for NasBN).
-    # It should use the default False and nn.BatchNorm correspondingly.
-    def __init__(self, input_size=224, n_class=1000, architecture=None, channel_scales=None,
-                 use_se=False, last_conv_after_pooling=False, shuffle_method=ShuffleChannels):
-        """
-        scale_cand_ids = [6, 5, 3, 5, 2, 6, 3, 4, 2, 5, 7, 5, 4, 6, 7, 4, 4, 5, 4, 3]
-        scale_candidate_list = [0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0]
-        stage_repeats = [4, 4, 8, 4]
-        len(scale_cand_ids) == sum(stage_repeats) == # feature blocks == 20
-        """
-        super(ShuffleNasOneShotFix, self).__init__(input_size=input_size, n_class=n_class,
-                                                   architecture=architecture, channel_scales=channel_scales,
-                                                   use_se=use_se, last_conv_after_pooling=last_conv_after_pooling,
-                                                   shuffle_method=shuffle_method)
-
-    def hybrid_forward(self, F, x, *args, **kwargs):
-        x = self.features(x)
-        x = self.output(x)
-        return x
+def make_divisible(x, divisible_by=8):
+    return int(np.ceil(x * 1. / divisible_by) * divisible_by)
 
 
-def get_shufflenas_oneshot(architecture=None, n_class=1000, scale_ids=None, use_all_blocks=False,
-                           use_se=False, last_conv_after_pooling=False, shuffle_by_conv=False):
+def get_shufflenas_oneshot(architecture=None, scale_ids=None, use_all_blocks=False, n_class=1000,
+                           use_se=False, last_conv_after_pooling=False, shuffle_by_conv=False,
+                           channels_layout='ShuffleNetV2+'):
+    if channels_layout == 'OneShot':
+        stage_out_channels = [64, 160, 320, 640]
+        candidate_scales = [0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0]
+        last_conv_out_channel = 1024
+    elif channels_layout == 'ShuffleNetV2+':
+        stage_out_channels = [48, 128, 256, 512]
+        candidate_scales = [0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6]
+        last_conv_out_channel = 1280
+    else:
+        raise ValueError("Unrecognized channels_layout: {}. "
+                         "Please choose from ['ShuffleNetV2', 'OneShot']".format(channels_layout))
+
     if architecture is None and scale_ids is None:
         # Nothing about architecture is specified, do random block selection and channel selection.
         net = ShuffleNasOneShot(n_class=n_class, use_all_blocks=use_all_blocks, bn=NasBatchNorm,
-                                use_se=use_se, last_conv_after_pooling=last_conv_after_pooling)
+                                use_se=use_se, last_conv_after_pooling=last_conv_after_pooling,
+                                stage_out_channels=stage_out_channels, candidate_scales=candidate_scales,
+                                last_conv_out_channel=last_conv_out_channel)
     elif architecture is not None and scale_ids is not None:
         # Create the specified structure
         if use_all_blocks:
@@ -316,7 +339,8 @@ def get_shufflenas_oneshot(architecture=None, n_class=1000, scale_ids=None, use_
             channel_scales.append(scale_list[scale_ids[i]])
         net = ShuffleNasOneShotFix(architecture=architecture, n_class=n_class, channel_scales=channel_scales,
                                    use_se=use_se, last_conv_after_pooling=last_conv_after_pooling,
-                                   shuffle_method=shuffle_method)
+                                   shuffle_method=shuffle_method, stage_out_channels=stage_out_channels,
+                                   candidate_scales=candidate_scales, last_conv_out_channel=last_conv_out_channel)
     else:
         raise ValueError("architecture and scale_ids should both be None for supernet "
                          "or both not None for fixed structure model.")
