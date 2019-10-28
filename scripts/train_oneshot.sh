9c9ebbe6fb532f2ac5ff37f65e5a01896149b989
Alex <kangyan.cn@gmail.com>
2019-10-28 16:33:05 -0700

    Update train_oneshot.sh

--- scripts/train_oneshot.sh
+++ scripts/train_oneshot.sh
@@ -10,4 +10,4 @@ python train_imagenet.py \
     --block-choices '0, 0, 3, 1, 1, 1, 0, 0, 2, 0, 2, 1, 1, 0, 2, 0, 2, 1, 3, 2' \
     --channel-choices '6, 5, 3, 5, 2, 6, 3, 4, 2, 5, 7, 5, 4, 6, 7, 4, 4, 5, 4, 3' \
     --channels-layout OneShot \
-    --save-dir params_shufflenas_oneshot+ --logging-file ./logs/shufflenas_oneshot+.log
+    --save-dir params_shufflenas_oneshot --logging-file ./logs/shufflenas_oneshot.log
