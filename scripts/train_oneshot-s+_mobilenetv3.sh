b33acacb337517643eadaaa787e5a015dfdf6b20
Alex <kangyan.cn@gmail.com>
2019-10-28 16:11:33 -0700

    Update train_oneshot-s+_mobilenetv3.sh

--- scripts/train_oneshot-s+_mobilenetv3.sh
+++ scripts/train_oneshot-s+_mobilenetv3.sh
@@ -6,8 +6,8 @@ python train_imagenet.py \
     --mode hybrid --lr 1.3 --wd 0.00003 --lr-mode cosine --dtype float16\
     --num-epochs 360 --batch-size 256 --num-gpus 4 -j 16 \
     --label-smoothing --no-wd --warmup-epochs 5 --use-rec \
-    --model ShuffleNas_fixArch 
-    --use-se --channels-layout ShuffleNetV2+ \
+    --model ShuffleNas_fixArch \
     --block-choices '0, 0, 0, 1, 0, 0, 1, 0, 3, 2, 0, 1, 2, 2, 1, 2, 0, 0, 2, 0' \
     --channel-choices '8, 7, 6, 8, 5, 7, 3, 4, 2, 4, 2, 3, 4, 5, 6, 6, 3, 3, 4, 6' \
+    --use-se --channels-layout ShuffleNetV2+ \
     --save-dir params_shufflenas_oneshot+_genetic --logging-file ./logs/shufflenas_oneshot+_genetic.log
