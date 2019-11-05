67dd650f1629d1c2ff2397e64b47ca6e41fd1770
Alex <kangyan.cn@gmail.com>
2019-11-05 14:28:08 -0800

    Create plot_genetic_search_blockAndChannel.sh

---
+++ scripts/plot_genetic_search_blockAndChannel.sh
@@ -0,0 +1,6 @@
+python utils/plot.py \
+    --log-file search_supernet_SinglePathOneShot.log \
+    --mode genetic_search \
+    --title 'Genetic Search Supernet Block-Channel' \
+    --save-dir './plots_blockAndChannel' \
+    --save-file genetic_search_supernet_blockAndChannel.png
