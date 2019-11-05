fe8ddc9e5896d60f95e77fb0acd78a1db4e6e0c4
Alex <kangyan.cn@gmail.com>
2019-11-05 14:19:49 -0800

    Create plot_genetic_search.sh

---
+++ scripts/plot_genetic_search.sh
@@ -0,0 +1,7 @@
+python utils/plot.py \
+    --log-file logs/genetic_search_supernet.log \
+    --mode genetic_search \
+    --title 'Genetic Search Supernet Only-block' \
+    --save-dir './plots_blockOnly' \
+    --save-file genetic_search_supernet_blockOnly.png \
+    --old-ver
