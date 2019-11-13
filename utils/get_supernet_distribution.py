a009de7ca5cdc3ffe75fe49569aa33b888ee3acc
Alex <kangyan.cn@gmail.com>
2019-11-13 14:32:09 -0800

    Add option for comparing original and se supernet

--- utils/get_supernet_distribution.py
+++ utils/get_supernet_distribution.py
@@ -19,18 +19,21 @@ def parse_args():
                         help='whether to follow MobileNet V3 last conv after pooling style')
     parser.add_argument('--channels-layout', type=str, default='OneShot',
                         help='The mode of channels layout: [\'ShuffleNetV2+\', \'OneShot\']')
-    parser.add_argument('--sample-count', type=int, default=217,
+    parser.add_argument('--sample-count', type=int, default=1000,
                         help='How many subnet to be sampled')
-    opts = parser.parse_args()
+    parser.add_argument('--compare', action='store_true',
+                        help='whether to plot the regular supernet and se supernet together to compare.')
+    opts = parser.parse_args('')
     return opts
 
 
-def get_flop_param_score(block_choices, channel_choices, comparison_model='SinglePathOneShot'):
+def get_flop_param_score(block_choices, channel_choices, comparison_model='SinglePathOneShot',
+                         use_se=False, last_conv_after_pooling=False, channels_layout='OneShot'):
     """ Return the flops and num of params """
     # build fix_arch network and calculate flop
     fixarch_net = get_shufflenas_oneshot(block_choices, channel_choices,
-                                         use_se=args.use_se, last_conv_after_pooling=args.last_conv_after_pooling,
-                                         channels_layout=args.channels_layout)
+                                         use_se=use_se, last_conv_after_pooling=last_conv_after_pooling,
+                                         channels_layout=channels_layout)
     fixarch_net._initialize()
     if not os.path.exists('./symbols'):
         os.makedirs('./symbols')
@@ -74,31 +77,67 @@ def get_flop_param_score(block_choices, channel_choices, comparison_model='Singl
 def get_distribution():
     net = get_shufflenas_oneshot(use_se=args.use_se, last_conv_after_pooling=args.last_conv_after_pooling,
                                  channels_layout=args.channels_layout)
+    if args.compare:
+        net = get_shufflenas_oneshot()
     print(net)
+    # TODO: find out why argparser does not work.
+    args.compare = True
+    print(args)
     flop_list = []
     param_list = []
+    se_flop_list = []
+    se_param_list = []
     for _ in range(args.sample_count):
         block_choices = net.random_block_choices(select_predefined_block=False)
         full_channel_mask, channel_choices = net.random_channel_mask(select_all_channels=False)
-        flops, model_size, _, _ = \
-                get_flop_param_score(block_choices, channel_choices)
-        flop_list.append(flops)
-        param_list.append(model_size)
+        if args.compare:
+            flops, model_size, _, _ = \
+                get_flop_param_score(block_choices, channel_choices, use_se=False, last_conv_after_pooling=False,
+                                     channels_layout=args.channels_layout)
+            flop_list.append(flops)
+            param_list.append(model_size)
+            se_flops, se_model_size, _, _ = \
+                get_flop_param_score(block_choices, channel_choices, use_se=True, last_conv_after_pooling=True,
+                                     channels_layout=args.channels_layout)
+            se_flop_list.append(se_flops)
+            se_param_list.append(se_model_size)
+        else:
+            flops, model_size, _, _ = \
+                get_flop_param_score(block_choices, channel_choices, use_se=args.use_se,
+                                     last_conv_after_pooling=args.last_conv_after_pooling,
+                                     channels_layout=args.channels_layout)
+            flop_list.append(flops)
+            param_list.append(model_size)
 
     # plot
-    plt.style.use("ggplot")
-    plt.figure()
-    axes = plt.gca()
-    # axes.set_xlim([-0.005, 0.5])
-    # axes.set_ylim([0.15, 0.7])
-    plt.scatter(flop_list, param_list, alpha=0.8, c='steelblue', s=50, label='subnet')
-    plt.title('Flops Param Distribution')
-    plt.xlabel("Flops")
-    plt.ylabel("Params amount")
-    plt.legend(loc="lower right")
-    plt.savefig('../images/supernet_flops_params_dist.png')
-    plt.show()
-    plt.close()
+    if args.compare:
+        plt.style.use("ggplot")
+        fig, (axs1, axs2) = plt.subplots(1, 2, sharex=True, sharey=True)
+        axs1.scatter(flop_list, param_list, alpha=0.8, c='mediumaquamarine', s=50, label='subnet')
+        axs1.set_title('Original SuperNet Distribution')
+        axs1.legend(loc="lower right")
+        axs2.scatter(se_flop_list, se_param_list, alpha=0.8, c='mediumaquamarine', s=50, label='subnet')
+        axs2.set_title('SE-SuperNet Distribution')
+        axs2.legend(loc="lower right")
+        for axs in [axs1, axs2]:
+            axs.set(xlabel='Flops', ylabel='Params amount')
+            # Hide x labels and tick labels for top plots and y ticks for right plots.
+            axs.label_outer()
+
+        plt.savefig('../images/supernet_flops_params_dist_compare.png')
+        plt.show()
+        plt.close()
+    else:
+        plt.style.use("ggplot")
+        plt.figure()
+        plt.scatter(flop_list, param_list, alpha=0.8, c='mediumaquamarine', s=50, label='subnet')
+        plt.title('Flops Param Distribution')
+        plt.xlabel("Flops")
+        plt.ylabel("Params amount")
+        plt.legend(loc="lower right")
+        plt.savefig('../images/supernet_flops_params_dist.png')
+        plt.show()
+        plt.close()
 
 
 if __name__ == '__main__':
