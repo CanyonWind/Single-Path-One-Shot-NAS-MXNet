151f4f2ab876e293fadd6b664534e9303424322f
Alex <kangyan.cn@gmail.com>
2019-11-15 14:24:17 -0800

    Add train with constraints example

---
+++ scripts/train_supernet+_with_constraints.sh
@@ -0,0 +1,12 @@
+export MXNET_SAFE_ACCUMULATION=1
+
+python train_imagenet.py \
+    --rec-train ~/imagenet/rec/train.rec --rec-train-idx ~/imagenet/rec/train.idx \
+    --rec-val ~/imagenet/rec/val.rec --rec-val-idx ~/imagenet/rec/val.idx \
+    --mode imperative --lr 0.65 --wd 0.00004 --lr-mode cosine --dtype float16\
+    --num-epochs 120 --batch-size 64 --num-gpus 1 -j 16 \
+    --label-smoothing --no-wd --warmup-epochs 5 --use-rec \
+    --model ShuffleNas \
+    --epoch-start-cs 0 --use-se --last-conv-after-pooling --channels-layout OneShot \
+    --save-dir params_shufflenas_supernet+ --logging-file ./logs/shufflenas_supernet+.log \
+    --train-upper-constraints 'flops-330-params-4.5'
